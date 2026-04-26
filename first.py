f68b89625c2a990cb5ad0a1f7ca3f38d42af3a70
AjeetCodes <ajeetyadav5544332211@gmail.com>
2026-04-26 21:23:39 +0530

    Update first.py
    Practicing new concepts in Python today.

--- first.py
+++ first.py
@@ -1,7 +1,21 @@
+print("Hello")
+channel_name = "MrFactz"
+daily_running_km = 6
+print("Hello, Welcome to", channel_name)
+print("My daily running target is:", daily_running_km, "km")
+import flask
+print("Flask is imported successfully!")
 import pyjokes
-# print("Printing Jokes...")
-print(pyjokes.get_joke())
+print("Pyjokes is imported successfully!")
+jokes = pyjokes.get_jokes()
+print(jokes)
+# welcome back to my channel
 """
 so thanks
 that was my program
 """
+'''
+so thanks
+that was my program
+'''
+
